2ca472a1410ce433df65ae8ebe0edb15806284f1
ProninaAnastasiia <soul_bler@mail.ru>
2023-11-29 16:11:10 +0300

    added controllers and pages for website view

---
+++ Auto.Website/Controllers/ModelsController.cs
@@ -0,0 +1,17 @@
+﻿using Auto.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auto.WebSite.Controllers;
+
+public class ModelsController : Controller {
+    private readonly IAutoDatabase db;
+
+    public ModelsController(IAutoDatabase db) {
+        this.db = db;
+    }
+
+    public IActionResult Index() {
+        var models = db.ListModels();
+        return View(models);
+    }
+}
---
+++ Auto.Website/Controllers/OwnersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Auto.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auto.WebSite.Controllers;
+
+public class OwnersController : Controller
+{
+    private readonly IAutoDatabase db;
+
+    public OwnersController(IAutoDatabase db) {
+        this.db = db;
+    }
+    
+    public IActionResult Owner(string id)
+    {
+        var owner = db.FindOwner(id);
+        Console.WriteLine(owner.Email);
+        return View(owner);
+    }
+    
+}
